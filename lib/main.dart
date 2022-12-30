04a02d2e769b175bac292fdbf34ecfe77a8c9dde
minhhanh34 <caubethieulam304@gmail.com>
2022-12-30 23:09:01 +0700

    tich hop firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:consultant/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-void main() => runApp(const ConsultantApp());
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ConsultantApp());
+}
 
 final _router = GoRouter(
   initialLocation: '/',
